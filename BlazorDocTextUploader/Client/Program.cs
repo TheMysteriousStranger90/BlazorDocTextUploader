baea5d397468cba69cef4cd34b2ac828c3ded910
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-09-13 22:09:43 +0300

    fix _baseUrl; deploy on Azure

--- BlazorDocTextUploader/Client/Program.cs
+++ BlazorDocTextUploader/Client/Program.cs
@@ -10,4 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5001/") });
+
 await builder.Build().RunAsync();
